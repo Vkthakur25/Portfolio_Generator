8fb6e2063afd1bd5af5d5c5f77e85338691fa692
Vivek kumar <vivek.kumarr166@gmail.com>
2025-07-29 14:22:57 +0530

    deploy2

--- portfolio-generator/src/main.jsx
+++ portfolio-generator/src/main.jsx
@@ -1,18 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App.jsx';
-import PreviewPage from './PreviewPage.jsx';
+import App from './App';
+import PreviewPage from './PreviewPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter basename="/Portfolio_Generator/">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/preview" element={<PreviewPage />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
